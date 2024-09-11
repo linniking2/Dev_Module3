777f876c2baa3abee9df86faa379c89c9f6b5b90
linniking2 <38794009+linniking2@users.noreply.github.com>
2024-09-11 14:40:22 +0300

    qq

--- src/test/java/SumCalculatorTest.java
+++ src/test/java/SumCalculatorTest.java
@@ -1,4 +1,5 @@
 import org.dev.devmodule3.SumCalculator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,9 +13,17 @@ public class SumCalculatorTest {
 
     @Test
     public void sumTest(){
-       
-        System.out.println(calculator.sum(3));
-        System.out.println(calculator.sum(1));
-        System.out.println(calculator.sum(0));
+       //When
+        int actual = calculator.sum(1);
+        int actual2 = calculator.sum(3);
+        //Then
+         int expected = 1;
+      int expected2 = 6;
+        Assertions.assertEquals(expected,actual);
+      Assertions.assertEquals(expected2,actual2);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            calculator.sum(0);
+        });
+
     }
 }
